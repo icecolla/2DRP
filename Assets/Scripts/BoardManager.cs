c0c2c06c8be9b504c99476dfca46a7c3d1da69af
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2026-01-22 22:06:38 +0300

    add: CellData in BoardManager

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,7 +3,13 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Passable;
+    }
+    
     private Tilemap _tilemap;
+    private CellData[,] _boardData;
 
     [SerializeField] private int _width;
     [SerializeField] private int _height;
@@ -13,20 +19,24 @@ public class BoardManager : MonoBehaviour
     void Start()
     {
         _tilemap = GetComponentInChildren<Tilemap>();
+        _boardData = new CellData[_width, _height];
 
         for (int y = 0; y < _height; y++)
         {
             for (int x = 0; x < _width; x++)
             {
                 Tile tile;
+                _boardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0 || x == _width - 1 || y == _height - 1)
                 {
                     tile = _wallTiles[Random.Range(0, _wallTiles.Length)];
+                    _boardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile =  _groundTiles[Random.Range(0, _groundTiles.Length)];
+                    _boardData[x, y].Passable = true;
                 }
 
                 _tilemap.SetTile(new Vector3Int(x, y, 0), tile);
