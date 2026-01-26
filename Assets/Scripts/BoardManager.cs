e78e61586cbc871d21127ddb39619e0651c95c9f
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2026-01-27 01:44:30 +0300

    add: food prefab and generation logic

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -6,16 +7,21 @@ public class BoardManager : MonoBehaviour
     public class CellData
     {
         public bool IsPassable;
+        public GameObject ContainedObject;
     }
     
     private Tilemap _tilemap;
     private CellData[,] _boardData;
     private Grid _grid;
 
+    private List<Vector2Int> _emptyCellList;
+
     [SerializeField] private int _boardWidth;
     [SerializeField] private int _boardHeight;
     [SerializeField] private Tile[] _groundTiles;
     [SerializeField] private Tile[] _wallTiles;
+    
+    [SerializeField] private GameObject _food;
     
     [SerializeField] private PlayerController Player;
 
@@ -23,6 +29,7 @@ public class BoardManager : MonoBehaviour
     {
         _tilemap = GetComponentInChildren<Tilemap>();
         _grid = GetComponentInChildren<Grid>();
+        _emptyCellList = new List<Vector2Int>();
         
         _boardData = new CellData[_boardWidth, _boardHeight];
 
@@ -42,11 +49,16 @@ public class BoardManager : MonoBehaviour
                 {
                     tile =  _groundTiles[Random.Range(0, _groundTiles.Length)];
                     _boardData[x, y].IsPassable = true;
+                    
+                    _emptyCellList.Add(new Vector2Int(x, y));
                 }
 
                 _tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+        
+        _emptyCellList.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 
     public Vector3 CellToWorldPosition(Vector2Int cellPosition)
@@ -58,5 +70,20 @@ public class BoardManager : MonoBehaviour
     {
         if (cellIndex.x <  0 || cellIndex.x >= _boardWidth || cellIndex.y >= _boardHeight) return null;
         return _boardData[cellIndex.x, cellIndex.y];
+    }
+
+    private void GenerateFood()
+    {
+        int foodCount = 5;
+        for (int i = 0; i < foodCount; ++i)
+        {
+            int randomIndex = Random.Range(0, _emptyCellList.Count);
+            Vector2Int cellPosition = _emptyCellList[randomIndex];
+            _emptyCellList.RemoveAt(randomIndex);
+            CellData data = _boardData[cellPosition.x, cellPosition.y];
+            GameObject newFood = Instantiate(_food);
+            newFood.transform.position = CellToWorldPosition(cellPosition);
+            data.ContainedObject = newFood;
+        }
     }
 }
